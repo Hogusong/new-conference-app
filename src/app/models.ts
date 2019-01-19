cdda0e2e69aef24ddc210d098c43ec60e2842c6e
“Young <hogusong1@gmail.com>
2019-01-19 18:14:14 -0500

    added Session and Speakers data model as interface in models.ts

--- src/app/models.ts
+++ src/app/models.ts
@@ -13,3 +13,30 @@ export interface TRACK {
   id?: string;
   name: string;
 }
+
+export interface Speaker {
+  id?: string;
+  name: string;
+  profilePic?: string;
+  twitter?: string;
+  github?: string;
+  instagram?: string;
+  about?: string;
+  location?: string;
+  email: string;
+  phone?: string;
+  sessions?: { id: string, name: string }[];   // session id & name
+}
+
+export interface Session {
+  id?: string;
+  name: string;
+  date: string;         // 2018-12-06
+  timeStart: string;    // 15:30 for 3:30pm
+  timeEnd?: string;
+  location?: string;
+  description?: string;
+  speakerIDs: string[];   // speaker's id
+  tracks: string[];     //  name of track
+  hide?: boolean;
+}
