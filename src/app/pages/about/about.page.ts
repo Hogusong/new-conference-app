909d78213c807a6742185ac961569d8a1f8808c0
“Young <hogusong1@gmail.com>
2019-01-21 21:03:22 -0500

    created UI of 'about.ts' in user mode: done completely all of the functions

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+import { PopoverPage } from '../about-popover/about-popover';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
-export class AboutPage implements OnInit {
+export class AboutPage {
+  conferenceDate = '2019-05-17';
 
-  constructor() { }
+  constructor(public popoverCtrl: PopoverController) { }
 
-  ngOnInit() {
+  async presentPopover(event: Event) {
+    const popover = await this.popoverCtrl.create({
+      component: PopoverPage,
+      event
+    });
+    await popover.present();
   }
-
 }
