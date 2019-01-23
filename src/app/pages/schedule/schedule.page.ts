7bd53c6f982a56034738f187eb545b423b464763
“Young <hogusong1@gmail.com>
2019-01-22 23:18:14 -0500

    added 'build all schedule' function  in 'schedule page' in app'

--- src/app/pages/schedule/schedule.page.ts
+++ src/app/pages/schedule/schedule.page.ts
@@ -4,6 +4,8 @@ import { LoadingController, ModalController } from '@ionic/angular';
 import { GeneralService } from '../../providers/general.service';
 import { FunctionService } from '../../providers/function.service';
 import { PeriodPage } from './period/period.page';
+import { SessionService } from 'src/app/providers/session.service';
+import { PARTOFDAY, SESSION, USER } from 'src/app/models';
 
 @Component({
   selector: 'app-schedule',
@@ -11,14 +13,29 @@ import { PeriodPage } from './period/period.page';
   styleUrls: ['./schedule.page.scss'],
 })
 export class SchedulePage implements OnInit{
+
   segment = '';
   queryText = '';
   startDate: string;
   endDate: string;
 
+  excludeTracks: any[] = [];
+  partsOfDay: PARTOFDAY[];
+  sessions: SESSION[];
+  schedule: {
+    date: string,
+    groups: {
+      indexKey: number
+      partOfDay: string,
+      sessions: any[]
+    }[]
+  }[] = [];
+  user: USER;
+ 
   constructor(private loadingCtrl: LoadingController,
               private modalCtrl: ModalController,
               private genService: GeneralService,
+              private sessionService: SessionService,
               private funService: FunctionService) { }
 
   ngOnInit() {
@@ -38,7 +55,71 @@ export class SchedulePage implements OnInit{
   }
 
   updateSchedule() {
-    console.log('update schedule')
+    this.schedule = [];
+    this.genService.getUser().then(user => {
+      user.trackFilter.forEach(track => {
+        if (!track.isChecked) { this.excludeTracks.push(track.name); }
+      });
+      this.user = user;
+      this.genService.getPartsOfDay().subscribe(
+        response => { this.partsOfDay = response ; }
+      );
+
+      this.sessionService.getSessionsInPeriod(this.startDate, this.endDate).subscribe(
+        (response: SESSION[]) => {
+          this.sessions = response;
+          this.buildSchedule();
+      });
+    });
+  }
+
+  getFilterOption() {
+    return {
+      queryText: this.queryText.toLowerCase().trim(),
+      segment: this.segment,
+      excludeTracks: this.excludeTracks
+    };
+  }
+
+  buildSchedule() {
+    const filterOption = this.getFilterOption();
+    this.sessions.forEach(session => {
+      session = this.sessionService.filterSession(session, filterOption);
+      const partOfDay = this.partsOfDay.find(part => part.timeFrom <= session.timeStart && part.timeTo >= session.timeStart);
+      const newGroup = {
+        indexKey: partOfDay.indexKey,
+        partOfDay: partOfDay.name,
+        sessions: [session]
+      };
+      const newItem = { date: session.date, groups: [newGroup]};
+
+      const sIndex = this.schedule.findIndex(item => item.date === session.date);
+      if (sIndex < 0) {
+        this.schedule.push(newItem);
+      } else {
+        const gIndex = this.schedule[sIndex].groups.findIndex(
+          group => group.partOfDay === partOfDay.name
+        );
+        if (gIndex < 0) {
+          this.schedule[sIndex].groups.push(newGroup);
+        } else {
+          this.schedule[sIndex].groups[gIndex].sessions.push(session);
+        }
+      }
+    });
+    this.schedule.sort((a, b) => {
+      if (a.date > b.date) { return 1; }
+      return -1;
+    });
+    this.schedule.forEach(item => {
+      item.groups.sort((a, b) => a.indexKey - b.indexKey);
+      item.groups.forEach(group => {
+        group.sessions.sort((a, b) => {
+          if (a.timeStart > b.timeStart) { return 1; }
+          return -1;
+        });
+      });
+    });
   }
 
   processBySegment() {
@@ -56,7 +137,14 @@ export class SchedulePage implements OnInit{
   }
 
   updateFilter() {
-    console.log('update filter')
+    const filterOption = this.getFilterOption();
+    this.schedule.forEach(daily => {
+      daily.groups.forEach(group => {
+        group.sessions.forEach(session => {
+          session = this.sessionService.filterSession(session, filterOption);
+        });
+      });
+    });
   }
 
   resetFilter() {
